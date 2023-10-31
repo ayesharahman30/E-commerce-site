09a8a7788d8b32ed09c18ad975d9ca68d9631d1b
abdur <52399506+arahman10000@users.noreply.github.com>
2023-11-01 00:57:44 +0600

    Delete tanvir-frontend-main directory

--- tanvir-frontend-main/src/components/utils/APIRoutes.js
+++
@@ -1,4 +0,0 @@
-export const host = "https://walmart-backend.vercel.app";
-export const allUsersRoute = `${host}/api/v1/allUsers`;
-export const sendMessageRoute = `${host}/api/v1/messages/addmsg`;
-export const recieveMessageRoute = `${host}/api/v1/messages/getmsg`;
